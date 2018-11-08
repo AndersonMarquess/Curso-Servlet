40401ce4fe31a376a5eeb9fae834f0f5a5d81496
AndersonMarquess <andersongabriel96.ag@gmail.com>
2018-11-08 16:02:08 -0200

    Criado filtro para verificar tempo decorrido
    Alterado tempo de vida da session para 60 minutos

---
+++ src/com/andersonmarques/filtros/Monitoramento.java
@@ -0,0 +1,29 @@
+package com.andersonmarques.filtros;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/")
+public class Monitoramento implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		//Inicio de um requisição
+		long inicio = System.currentTimeMillis();
+
+		//Continua a requisição
+		chain.doFilter(request, response);
+		
+		//Fim de uma requisição
+		long fim = System.currentTimeMillis();
+		System.out.println("Tempo decorrido: "+(fim-inicio)+" ms, para o endpoint: "+request.getParameter("acao"));
+	}
+}
